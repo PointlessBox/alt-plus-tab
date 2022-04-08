e7a4970030c925217609cccf830b9a907be11dcd
f-kraft <fabian.kraft@protonmail.com>
2022-04-08 11:10:38 +0200

    * Restructured test for RingSet

--- src/ring-set/ring-set.spec.ts
+++ src/ring-set/ring-set.spec.ts
@@ -3,60 +3,52 @@ import { expect } from "chai";
 import RingSet from "./ring_set";
 
 describe("RingSet", () => {
-    describe("if a value does NOT already exist in the RingSet", () => {
-        describe("append(value)", () => {
-            it("should append the value", () => {
-                const ringSet = new RingSet([1, 2])
-                
-                ringSet.append(3)
-                
-                const iterator = ringSet.iterator()
-                iterator.next() // skip 1
-                iterator.next() // skip 2
-                expect(iterator.next().value).to.equal(3)
-            })
+    describe("append(value)", () => {
+        it("should append the value if it does NOT exists already", () => {
+            const ringSet = new RingSet([1, 2])
+            
+            ringSet.append(3)
+            
+            const iterator = ringSet.iterator()
+            iterator.next() // skip 1
+            iterator.next() // skip 2
+            expect(iterator.next().value).to.equal(3)
         })
-        describe("prepend(value)", () => {
-            it("should prepend a value at the start", () => {
-                const ringSet = new RingSet([1, 2])
+        it("should remove the given value from it's current position and append it at the end", () => {
+            const ringSet = new RingSet([3, 1, 2])
+            
+            ringSet.append(3)
 
-                ringSet.prepend(3)
+            ringSet.forEach((val) => console.log(val))
 
-                const iterator = ringSet.iterator()
-                expect(iterator.next().value).to.equal(3) // prepended value
-                expect(iterator.next().value).to.equal(1)
-                expect(iterator.next().value).to.equal(2)
-            })
+            const iterator = ringSet.iterator()
+            expect(iterator.next().value).to.equal(1) // '3' should be moved to end
+            expect(iterator.next().value).to.equal(2)
+            expect(iterator.next().value).to.equal(3)
+            expect(iterator.next().value).to.equal(undefined)
         })
     })
-    describe("if a value DOES already exist in the RingSet", () => {
-        describe("append(value)", () => {
-            it("should remove the given value from it's current position and append it at the end", () => {
-                const ringSet = new RingSet([3, 1, 2])
-                
-                ringSet.append(3)
+    describe("prepend(value)", () => {
+        it("should prepend a value at the start if it does NOT exists already", () => {
+            const ringSet = new RingSet([1, 2])
 
-                ringSet.forEach((val) => console.log(val))
+            ringSet.prepend(3)
 
-                const iterator = ringSet.iterator()
-                expect(iterator.next().value).to.equal(1) // '3' should be moved to end
-                expect(iterator.next().value).to.equal(2)
-                expect(iterator.next().value).to.equal(3)
-                expect(iterator.next().value).to.equal(undefined)
-            })
+            const iterator = ringSet.iterator()
+            expect(iterator.next().value).to.equal(3) // prepended value
+            expect(iterator.next().value).to.equal(1)
+            expect(iterator.next().value).to.equal(2)
         })
-        describe("prepend(value)", () => {
-            it("should remove the given value from it's current position and prepend it at the end", () => {
-                const ringSet = new RingSet([2, 3, 1])
+        it("should remove the given value from it's current position and prepend it at the end", () => {
+            const ringSet = new RingSet([2, 3, 1])
 
-                ringSet.prepend(1)
+            ringSet.prepend(1)
 
-                const iterator = ringSet.iterator()
-                expect(iterator.next().value).to.equal(1) // prepended value
-                expect(iterator.next().value).to.equal(2)
-                expect(iterator.next().value).to.equal(3)
-                expect(iterator.next().value).to.equal(undefined) // '1' should be moved to the start
-            })
+            const iterator = ringSet.iterator()
+            expect(iterator.next().value).to.equal(1) // prepended value
+            expect(iterator.next().value).to.equal(2)
+            expect(iterator.next().value).to.equal(3)
+            expect(iterator.next().value).to.equal(undefined) // '1' should be moved to the start
         })
     })
 })
